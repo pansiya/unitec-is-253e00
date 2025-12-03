dc6727f56bcd16ea1881e187ababb7f1ba60bcb1
Daniel Carmona <dancar726@gmail.com>
2025-12-02 23:36:50 -0600

    Cambiando costado en program.cs

--- Project3/src/ConsoleApp/Program.cs
+++ Project3/src/ConsoleApp/Program.cs
@@ -5,7 +5,7 @@ double basemayor = 20;
 double perimetro = Application.Trapecio251E13.Perimetro(costados, basem, basemayor);
 double area = Application.Trapecio251E13.Area(basem, basemayor, altura);
 double volumen = Application.Trapecio251E13.Volumen(area, altura);
-Console.WriteLine($"El perímetro de un trapecio de costados de {costados}cm con una base menor de {basem}cm y base mayor de {basemayor}cm es {perimetro}cm");
+Console.WriteLine($"El perímetro de un trapecio con costado de {costados}cm con una base menor de {basem}cm y base mayor de {basemayor}cm es {perimetro}cm");
 Console.WriteLine($"El área de un trapecio de base menor {basem}cm por base mayor {basemayor}cm de altura {altura}cm es {area}cm2");
 Console.WriteLine($"El volumen de un trapecio de area de {area}cm2 y altura de {altura}cm es {volumen}cm3");
 
